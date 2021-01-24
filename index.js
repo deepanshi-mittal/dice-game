0f1e0f358eb32a61914f64a3b3b43226c18c1f47
Deepanshi Mittal <deepanshimittal1998@gmail.com>
2021-01-24 13:44:10 +0530

    files changed

--- index.js
+++ index.js
@@ -1,37 +1,67 @@
-const slides = document.getElementsByClassName('carousel-item');
-let slidePosition = 0;
-const totalSlides = slides.length;
+// Create variables for the game state
+let player1Score = 0
+let player2Score = 0
+let player1Turn = true
 
-document.getElementById('carousel-button-next').addEventListener('click', moveToNextSlide);
-document.getElementById('carousel-button-prev').addEventListener('click', moveToPrevSlide);
+// Create variables to store references to the necessary DOM nodes
+const player1Dice = document.getElementById("player1Dice")
+const player2Dice = document.getElementById("player2Dice")
+const player1Scoreboard = document.getElementById("player1Scoreboard")
+const player2Scoreboard = document.getElementById("player2Scoreboard")
+const message = document.getElementById("message")
+const rollBtn = document.getElementById("rollBtn")
+const resetBtn = document.getElementById("resetBtn")
 
-function hideAllSlides() {
-    for (let slide of slides) {
-        slide.classList.remove('carousel-item-visible');
-        slide.classList.add('carousel-item-hidden');
-    }
+function showResetButton() {
+    rollBtn.style.display = "none"
+    resetBtn.style.display = "block"
 }
 
-function moveToNextSlide() {
-    hideAllSlides();
-    
-    if (slidePosition === totalSlides - 1) {
-        slidePosition = 0;
-    } else {
-        slidePosition++;
-    }
-    
-    slides[slidePosition].classList.add("carousel-item-visible");
-}
+/* Hook up a click event listener to the Roll Dice Button. */
+ rollBtn.addEventListener("click", function() {
+    const randomNumber = Math.floor(Math.random() * 6) + 1
 
-function moveToPrevSlide() {
-    hideAllSlides();
-    
-    if (slidePosition === 0) {
-        slidePosition = totalSlides - 1;
+    if (player1Turn) {
+        player1Score += randomNumber
+        player1Scoreboard.textContent = player1Score
+        player1Dice.textContent = randomNumber
+        player1Dice.classList.remove("active")
+        player2Dice.classList.add("active")
+        message.textContent = "Player 2 Turn"
     } else {
-        slidePosition--;
+        player2Score += randomNumber
+        player2Scoreboard.textContent = player2Score
+        player2Dice.textContent = randomNumber
+        player2Dice.classList.remove("active")
+        player1Dice.classList.add("active")
+        message.textContent = "Player 1 Turn"
     }
     
-    slides[slidePosition].classList.add("carousel-item-visible");
+    if (player1Score >= 20) {
+        message.textContent = "Player 1 Won 🥳"
+        showResetButton()
+    }  else if (player2Score >= 20) {
+        message.textContent = "Player 2 Won 🎉"
+        showResetButton()
+    }
+    player1Turn = !player1Turn
+})
+ 
+resetBtn.addEventListener("click", function(){
+    reset()
+})
+
+function reset() {
+    player1Score = 0
+    player2Score = 0
+    player1Turn = true
+    player1Scoreboard.textContent = 0
+    player2Scoreboard.textContent = 0
+    player1Dice.textContent = "-"
+    player2Dice.textContent = "-"
+    message.textContent = "Player 1 Turn"
+    resetBtn.style.display = "none"
+    rollBtn.style.display = "block"
+    player2Dice.classList.remove("active")
+    player1Dice.classList.add("active")
 }
